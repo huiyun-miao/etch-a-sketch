f77714d9ac687ee62cf91591fb370a6b88c170a1
Huiyun Miao <109335165+huiyun-miao@users.noreply.github.com>
2026-02-09 19:22:48 +0200

    Add a prompt to choose the grid number

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const container = document.querySelector(".container");
-
-let gridNumber = 16;
 let drawing = false;
+let gridNumber = Number(
+  prompt("Please choose a number between 10 and 100 for the grid.", "16"),
+);
+const canvasWidth = 600;
+const container = document.querySelector(".container");
 
 for (let i = 0; i < gridNumber * gridNumber; i++) {
   const grid = document.createElement("div");
   grid.id = `grid${i}`;
-  grid.style.width = grid.style.height = `${600 / gridNumber}px`;
-  grid.style.border = "0.7px solid black";
+  grid.style.width = grid.style.height = `${canvasWidth / gridNumber}px`;
+  grid.style.border = "0.1px solid black";
   grid.style.boxSizing = "border-box";
   container.appendChild(grid);
 
   grid.addEventListener("mousedown", () => {
     drawing = true;
-    grid.style.background = "black";
+    grid.style.backgroundColor = "black";
   });
   grid.addEventListener("mousemove", () => {
     if (!drawing) return;
-    grid.style.background = "black";
+    grid.style.backgroundColor = "black";
   });
   document.addEventListener("mouseup", () => {
     drawing = false;
